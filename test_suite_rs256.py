9399a1341a9ec157ba28a2376febd440e228bc1c
Herron, James <jamesherron@my.unt.edu>
2024-09-22 22:40:05 -0500

    formatting

--- test_suite_rs256.py
+++ test_suite_rs256.py
@@ -9,24 +9,24 @@ def client():
         yield test
 
 # test for proper status code on auth page
-def test_Valid_JWT_authentication(client):
+def test_valid_auth_code(client):
     test = client.post("/auth")
     assert test.status_code == 200
 
 # test to see if jwt has and exp value
-def test_Expired_JWT_authentication(client):
+def test_exp_value(client):
     test = client.post("/auth")
     data = jwt.decode(test.get_json().get("token"), public_key, "RS256")
     assert data['exp'] is not None
 
 # checks if valid token is in the jwks
-def test_Valid_JWK_Found_In_JWKS(client):
+def test_valid_found(client):
     test = client.post("/auth")
     header = jwt.get_unverified_header(test.get_json().get("token"))
     assert header.get("kid") in [key["kid"] for key in client.get("/.well-known/jwks.json").get_json()["keys"]]
 
 # checks if invalid tokens are in the jwks
-def test_Expired_JWK_Not_Found_In_JWKS(client):
+def test_invalid_found(client):
     test = client.post("/auth?expired=true")
     header = jwt.get_unverified_header(test.get_json().get("token"))
     assert header.get("kid") not in [key["kid"] for key in client.get("/.well-known/jwks.json").get_json()["keys"]]
